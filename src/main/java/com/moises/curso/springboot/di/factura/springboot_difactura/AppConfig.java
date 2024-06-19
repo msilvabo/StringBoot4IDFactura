6972b98f6fe4b8ebe3d12479fbbb89136fedb0fc
Moises Silva Cardenas <moises.silva@et.bo>
2024-06-19 11:43:29 -0400

    72. Registrando otras lineas para la factura

--- src/main/java/com/moises/curso/springboot/di/factura/springboot_difactura/AppConfig.java
+++ src/main/java/com/moises/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.moises.curso.springboot.di.factura.springboot_difactura.models.Item;
@@ -20,7 +21,16 @@ public class AppConfig {
         Product p1 = new Product("Camara Sony", 100);
         Product p2 = new Product("TV Samsung", 350);
         Product p3 = new Product("Bicicleta", 150);
-
+        
         return Arrays.asList(new Item(p1,2),new Item(p2,5),new Item(p3,10));
+    }
+    
+    @Bean
+    @Primary
+    List<Item> itemsInvoiceOffice(){
+        Product p1 = new Product("monitor Asus", 300);
+        Product p2 = new Product("noteBook", 1150);
+
+        return Arrays.asList(new Item(p1,4),new Item(p2,6));
     }
 }
--- src/main/java/com/moises/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ src/main/java/com/moises/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -3,6 +3,7 @@ package com.moises.curso.springboot.di.factura.springboot_difactura.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +12,11 @@ public class Invoice {
     @Autowired
     private Client client;
 
-    @Value("${invoice.description}")    
+    @Value("${invoice.description.office}")    
     private String description;
 
     @Autowired
+    // @Qualifier("itemsInvoiceOffice")
     private List<Item> items;
 
     public Client getClient() {
